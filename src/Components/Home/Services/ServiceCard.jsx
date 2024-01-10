88c0642ac550dba86faa52d2f5c65cd6b0df3c21
Shahriar090 <shahriarh655@gmail.com>
2024-01-10 19:42:12 +0600

    Testing

--- src/Components/Home/Services/ServiceCard.jsx
+++ src/Components/Home/Services/ServiceCard.jsx
@@ -9,11 +9,11 @@ const ServiceCard = ({ singleService }) => {
       </picture> */}
       <div className="card-body gap-4">
         <div className="service">
-          <h2 className="card-title text-[#05845a] font-serif text-xl md:text-xl lg:text-2xl bg-[#ecfef4] px-2  rounded-lg">
+          <h2 className="card-title text-[#ECFEF4] font-serif text-xl md:text-xl lg:text-2xl bg-[#04724D] px-2  rounded-lg">
             {service}
           </h2>
         </div>
-        <p className="font-sans text-lg text-black">{description}</p>
+        <p className="font-sans text-xl text-black">{description}</p>
         <div className="card-actions justify-start ">
           <button className="btn btn-outline rounded-full bg-[#05845a] text-[#ecfef4] border-0 font-sans font-semibold  hover:bg-[#17986d] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110">
             Contact Now
